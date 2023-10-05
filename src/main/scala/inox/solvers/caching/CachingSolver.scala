76470a3896d6386216e0f23b75b1b492fa41d78f
Samuel Chassot <14821693+samuelchassot@users.noreply.github.com>
2023-10-05 05:42:41 +0000

    continue working on the cache

--- src/main/scala/inox/solvers/caching/CachingSolver.scala
+++ src/main/scala/inox/solvers/caching/CachingSolver.scala
@@ -16,20 +16,46 @@ class CachingSolver private (
   ) =
     this(underlying.context)(underlying)
 
+  var cache: Cache = MapCache()
+
   def name: String = "Caching:" + underlying.name
 
-  def declare(vd: ValDef) = underlying.declare(vd)
+  def declare(vd: ValDef): Unit = underlying.declare(vd)
 
-  def assertCnstr(expr: Expr) = underlying.assertCnstr(expr)
+  def assertCnstr(expr: Expr): Unit = underlying.assertCnstr(expr)
 
-  def check(config: CheckConfiguration) =
+  def check(
+      config: CheckConfiguration
+  ): config.Response[CachingSolver.this.Model, Assumptions] =
     underlying.check(config)
-  def checkAssumptions(config: Configuration)(assumptions: Set[Expr]) =
+  def checkAssumptions(config: Configuration)(
+      assumptions: Set[Expr]
+  ): config.Response[CachingSolver.this.Model, Assumptions] =
     underlying.checkAssumptions(config)(assumptions)
 
-  def interrupt() = underlying.interrupt()
-  def free() = underlying.free()
-  def reset() = underlying.reset()
-  def push() = underlying.push()
-  def pop() = underlying.pop()
+  def interrupt(): Unit = underlying.interrupt()
+  def free(): Unit = underlying.free()
+  def reset(): Unit = underlying.reset()
+  def push(): Unit = underlying.push()
+  def pop(): Unit = underlying.pop()
+}
+
+trait Key {}
+trait Value {}
+
+trait Cache {
+  def insert(key: Key, value: Value): Unit
+  def lookup(key: Key): Option[Value]
+}
+
+class MapCache extends Cache {
+  var cache: Map[Key, Value] = Map()
+
+  def insert(key: Key, value: Value): Unit = {
+    cache = cache + (key -> value)
+  }
+
+  def lookup(key: Key): Option[Value] = {
+    cache.get(key)
+  }
 }
--- src/main/scala/inox/solvers/caching/CachingSolverTrait.scala
+++ src/main/scala/inox/solvers/caching/CachingSolverTrait.scala
@@ -2,22 +2,22 @@ package inox
 package solvers
 package cachinng
 
-trait CachingSolverT extends Solver {
-  import program.trees._
-  import SolverResponses._
+// trait CachingSolverT extends Solver {
+//   import program.trees._
+//   import SolverResponses._
 
-  override def name: String = "Caching:" + super.name
+//   override def name: String = "Caching:" + super.name
 
-  def assertCnstr(expr: Expr) = underlying.assertCnstr(expr)
+//   def assertCnstr(expr: Expr) = underlying.assertCnstr(expr)
 
-  def check(config: CheckConfiguration) =
-    underlying.check(config)
-  def checkAssumptions(config: Configuration)(assumptions: Set[Expr]) =
-    underlying.checkAssumptions(config)(assumptions)
+//   def check(config: CheckConfiguration) =
+//     underlying.check(config)
+//   def checkAssumptions(config: Configuration)(assumptions: Set[Expr]) =
+//     underlying.checkAssumptions(config)(assumptions)
 
-  def interrupt() = underlying.interrupt()
-  def free() = underlying.free()
-  def reset() = underlying.reset()
-  def push() = underlying.push()
-  def pop() = underlying.pop()
-}
+//   def interrupt() = underlying.interrupt()
+//   def free() = underlying.free()
+//   def reset() = underlying.reset()
+//   def push() = underlying.push()
+//   def pop() = underlying.pop()
+// }
